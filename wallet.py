0e85c96b71de0358bf7e04624ed979554dc9d0bc
luccatatoni <80715146+luccatatoni@users.noreply.github.com>
2021-08-04 16:02:23 -0400

    2nd

--- wallet.py
+++ wallet.py
@@ -1,22 +1,17 @@
 # Import dependencies
 import subprocess
 import json
-from dotenv import load_dotenv
 from constants import *
-
-
-import subprocess
-import json
+from dotenv import load_dotenv
 import os
-from constants import *
-from dotenv import load_dotenv
-#from bipwallet import wallet
 from web3 import Web3
 from eth_account import Account
+from bit import wif_to_key
+load_dotenv()
 from bit import PrivateKeyTestnet
 from bit.network import NetworkAPI
+from web3.auto.gethdev import w3
 from web3.middleware import geth_poa_middleware
-from web3.gas_strategies.time_based import medium_gas_price_strategy
 
 # Load and set environment variables
 load_dotenv("LT.env")
@@ -35,48 +30,48 @@ def derive_wallets(mnemonic, coin, numderive):
     return json.loads(output)
 
 # Create a dictionary object called coins to store the output from `derive_wallets`.
-coins = derive_wallets(mnemonic,'ETH', 3)
+coins = derive_wallets(mnemonic, "ETH", 3),
+print(coins)
 
+# # Create a function called `priv_key_to_account` that converts privkey strings to account objects.
+# def priv_key_to_account(coin,priv_key):
+#     print(coin)
+#     print(priv_key)
+#     if coin == ETH:
+#         return Account.privateKeyToAccount(priv_key)
+#     elif coin == BTCTEST:
+#         return PrivateKeyTestnet(priv_key)
 
-# Create a function called `priv_key_to_account` that converts privkey strings to account objects.
-def priv_key_to_account(coin,priv_key):
-    print(coin)
-    print(priv_key)
-    if coin == ETH:
-        return Account.privateKeyToAccount(priv_key)
-    elif coin == BTCTEST:
-        return PrivateKeyTestnet(priv_key)
-
-# Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
-def create_tx(coin,account, recipient, amount):
-    if coin == ETH: 
-        gasEstimate = w3.eth.estimateGas(
-            {"from":eth_acc.address, "to":recipient, "value": amount}
-        )
-        return { 
-            "from": eth_acc.address,
-            "to": recipient,
-            "value": amount,
-            "gasPrice": w3.eth.gasPrice,
-            "gas": gasEstimate,
-            "nonce": w3.eth.getTransactionCount(eth_acc.address)
-        }
+# # Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
+# def create_tx(coin,account, recipient, amount):
+#     if coin == ETH: 
+#         gasEstimate = w3.eth.estimateGas(
+#             {"from":eth_acc.address, "to":recipient, "value": amount}
+#         )
+#         return { 
+#             "from": eth_acc.address,
+#             "to": recipient,
+#             "value": amount,
+#             "gasPrice": w3.eth.gasPrice,
+#             "gas": gasEstimate,
+#             "nonce": w3.eth.getTransactionCount(eth_acc.address)
+#         }
     
-    elif coin == BTCTEST:
-        return PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)])
+#     elif coin == BTCTEST:
+#         return PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)])
 
 
-# Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
-def send_txn(coin,account,recipient, amount):
-    txn = create_tx(coin, account, recipient, amount)
-    if coin == ETH:
-        signed_txn = eth_acc.sign_transaction(txn)
-        result = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-        print(result.hex())
-        return result.hex()
-    elif coin == BTCTEST:
-        tx_btctest = create_tx(coin, account, recipient, amount)
-        signed_txn = account.sign_transaction(txn)
-        print(signed_txn)
-        return NetworkAPI.broadcast_tx_testnet(signed_txn)
+# # Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
+# def send_txn(coin,account,recipient, amount):
+#     txn = create_tx(coin, account, recipient, amount)
+#     if coin == ETH:
+#         signed_txn = eth_acc.sign_transaction(txn)
+#         result = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+#         print(result.hex())
+#         return result.hex()
+#     elif coin == BTCTEST:
+#         tx_btctest = create_tx(coin, account, recipient, amount)
+#         signed_txn = account.sign_transaction(txn)
+#         print(signed_txn)
+#         return NetworkAPI.broadcast_tx_testnet(signed_txn)
 
